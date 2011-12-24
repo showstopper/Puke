3755e982481978dcf67271f69f03aa385141f3d8
Yannic Ahrens <yannic@ofmlabs.org>
2011-12-24 16:09:47 +0100

    Easy task made easy - we're only looking for a sequence of 11 set bits.

--- demux2.py
+++ demux2.py
@@ -3,21 +3,25 @@
 
 from bitstring import BitStream
 
+FRAME_SYNC = "0b" + "1" * 11 # 11 bits set
 def do_sync(stream):
-    while True:
-        pos = stream.find("0xff") # adjusts position to occurence implicitly
-        if not pos:
-            print "Did not succeed in finding a sync-frame."
-            return False
-        elif stream.read(11) == "0b%s" % '1' * 11: # 11 bits set
-            print "Found sync-frame at %s." % pos
-            return True
+    pos = stream.find(FRAME_SYNC, stream.pos) # adjusts position to occurence implicitly
+    if pos:
+        print "Found sync at offset=%s" % pos
+        return True
+    print "Did not succeed in finding a sync-frame."
+    return False
 
 def test_syncing():
-    valid_sync = BitStream("0x00fffb")
+    valid_sync = BitStream("0b0"+'1'*11)
+    valid_sync2 = BitStream("0xff00fffb")
     invalid_sync = BitStream("0x0000")
+    invalid_sync2 = BitStream("0xff00")
+    # Remember: skip the 11 bits
     assert do_sync(valid_sync)
+    assert do_sync(valid_sync2)
     assert not do_sync(invalid_sync)
+    assert not do_sync(invalid_sync2)
 
 def main():
     test_syncing()
